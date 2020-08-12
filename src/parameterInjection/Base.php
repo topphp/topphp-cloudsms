0566a0bb0c37b4b7315cd77f23ede6d1abcd3464
houlinlin <houlinlin@kaituocn.com>
2020-08-12 10:04:26 +0800

    凯拓云短信

---
+++ src/parameterInjection/Base.php
@@ -0,0 +1,176 @@
+<?php
+
+/**
+ * 短信发送接口
+ */
+
+namespace Tophp\TophpCloudsms\parameterInjection;
+
+use Lizhijun\Des3\Des3;
+
+class Base
+{
+    protected $url; #url地址
+    protected $iv;#加密
+    protected $account;#账号
+    protected $password;#密码
+    protected $key;
+
+
+    /**
+     * @return mixed
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIv()
+    {
+        return $this->iv;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAccount()
+    {
+        return $this->account;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getKey()
+    {
+        return $this->key;
+    } #key密钥
+
+
+    /**
+     * @param string $url
+     * @return $this
+     * @author Administrator
+     */
+    public function setUrl($url = '')
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+
+    /**
+     * @param string $iv
+     * @return $this
+     * @author Administrator
+     */
+    public function setIv($iv = '')
+    {
+        $this->iv = $iv;
+        return $this;
+    }
+
+
+    /**
+     * @param string $account
+     * @return $this
+     * @author Administrator
+     */
+    public function setAccount($account = '')
+    {
+        $this->account = $account;
+        return $this;
+    }
+
+
+    /**
+     * @param string $key
+     * @return $this
+     * @author Administrator
+     */
+    public function setKey($key = '')
+    {
+        $this->key = $key;
+        return $this;
+    }
+
+
+    /**
+     * @param string $password
+     * @return $this
+     * @throws \Lizhijun\Des3\Exceptions\InvalidArgumentException
+     * @author Administrator
+     */
+    public function setPassword($password = '')
+    {
+        /**
+         * Des3加密
+         * @param $password
+         * @return string
+         * @throws \Lizhijun\Des3\Exceptions\InvalidArgumentException
+         */
+        $des = new Des3($this->key, $this->iv);
+        $this->password = $des->encrypt($password);
+        return $this;
+    }
+
+    /**
+     * @param $func
+     * @param $jsonStr
+     * @return array
+     */
+    public function sendPost($func, $jsonStr = [])
+    {
+        if (!$this->getUrl()) {
+            return $this->msgAttr($code = 301, 'url地址必填');
+        }
+
+
+        if (isset($jsonStr['condition']['startTime']) && $jsonStr['condition']['startTime']) {
+            $jsonStr['condition']['startTime'] = date(DATE_ISO8601, $jsonStr['condition']['startTime']);
+        }
+        if (isset($jsonStr['condition']['endTime']) && $jsonStr['condition']['endTime']) {
+            $jsonStr['condition']['endTime'] = date(DATE_ISO8601, $jsonStr['condition']['endTime']);
+        }
+
+        if (isset($jsonStr['condition']['date']) && $jsonStr['condition']['date']) {
+            $jsonStr['condition']['date'] = date(DATE_ISO8601, $jsonStr['condition']['date']);
+        }
+
+        $data['account'] = $this->getAccount();
+        $data['password'] = $this->getPassword();
+        $jsonStr = array_merge(array_filter($jsonStr), $data);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_URL, $this->url . '/client/' . $func);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($jsonStr));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json; charset=utf-8',
+                'Content-Length: ' . strlen(json_encode($jsonStr))
+            )
+        );
+        $response = curl_exec($ch);
+        curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        return json_decode($response, true);
+    }
+
+    public function msgAttr($msg,$code = 301)
+    {
+        $data['code'] = $code;
+        $data['msg'] = $msg . '必填值';
+        return json_encode($data);
+    }
+}
---
+++ tests/ExampleTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Topphp\Test;
+
+use PHPUnit\Framework\TestCase;
+use Tophp\TophpCloudsms\ShortMessage;
+
+class ExampleTest extends TestCase
+{
+
+
+    public function testAc()
+    {
+
+        $account = 'xxx';
+        $password = 'xxx';
+        $key = 'xxxx';
+        $iv = 'xxxx';
+        $url = 'xxxxxx';
+        $ShortMessage = new ShortMessage();
+        $ShortMessage->setUrl($url)->setKey($key)->setIv($iv)->setAccount($account)->setPassword($password);
+        #发送短信
+        //$arr=$ShortMessage
+        //    ->setTemplateParam("{code:1223}")
+        //    ->setCode(86)
+        //    ->setMobile('15332097358')
+        //    ->setSignatureId("25")
+        //    ->setTemplateId("36")
+        //    ->sendMessage();
+        echo '<pre>';
+        #验证下游
+        $arr = $ShortMessage->authenticateCompany();
+        print_r($arr);
+        #获取签名集合
+        $arr = $ShortMessage->listSignatures();
+        print_r($arr);
+        #获取签名
+        $signatureId = 26;
+        $arr = $ShortMessage->getSignature($signatureId);
+        print_r($arr);
+        #获取模板集合
+        $arr = $ShortMessage->listTemplates();
+        print_r($arr);
+        //获取模板
+        $templateId = '38';
+        $arr = $ShortMessage->getTemplate($templateId);
+        print_r($arr);
+        #获取支付配置
+        $arr = $ShortMessage->getPaySettingData();
+        print_r($arr);
+
+        #获取支付日志集合
+        $arr = $ShortMessage->setPageNum(1)->setPageSize(1)->setStartTime('')->setEndTime(time())->listCompanyPayLogs();
+        print_r($arr);
+        #获取支付记录
+        $orderNo = 'N20200717150058810220';
+        $arr = $ShortMessage->getCompanyPayLog($orderNo);
+        print_r($arr);
+        #获取短信日志集合
+        $arr = $ShortMessage
+            ->setPageNum(1)
+            ->setPageSize(1)
+            ->setType(1)
+            ->setPhone('')
+            ->setDate('') #当天的数据
+            ->listCompanySmsLogs();
+        print_r($arr);
+        #国内套餐参数
+        $arr = $ShortMessage->setPayPlanId(21)->setPayType(1)->companyPaySetMeal();
+        print_r($arr);
+        #国内通过短信数量收费
+        $arr = $ShortMessage->setCount(21)->setPayType(2)->companyPaySetMealNumber();
+        print_r($arr);
+        #国际只能通过输入多少钱购买
+        $arr = $ShortMessage->setMoney(5000)->setPayType(2)->companyPaySetMealMoney();
+        print_r($arr);
+
+
+        #获取下游信息
+        $arr = $ShortMessage->getCompanyInfo();
+
+
+        print_r($arr);
+
+    }
+}
